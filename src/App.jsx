cf328ef9dcfd384fbc8eb6f6276ca465d8268259
Joy He <joyhe520@gmail.com>
2023-06-20 16:43:57 -0500

    display recommended coffee on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React, {useState, useEffect} from 'react'
 import axios from "axios";
-import {useSelector, useDispatch} from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
 import { fetch_recommended } from './components/reducers/exampleSlice';
+
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Tooltip,
+} from "@material-tailwind/react";
 
 
 const App = () => {
   const dispatch = useDispatch()
+  
   const recommendedCoffeeRecipeData = useSelector(state => state.coffeeReducer.recommendedList)
- 
+//  const [recommendCoffee, setRecommendCoffee] = useState([])
+let coffeeDataFromRedux
+
 
 
 
@@ -769,10 +781,12 @@ const App = () => {
           console.log(fetchCoffeeData.data.hits)
           const exampleData = fetchCoffeeData.data.hits
           dispatch(fetch_recommended(exampleData))
+
+
+         
+         
         }
-     
-        
-       console.log(recommendedCoffeeRecipeData[0])
+       
       } catch (error) {
         console.log(error)
       }
@@ -782,7 +796,9 @@ const App = () => {
    
     fetchRecommendedCoffee()
   }, [])
-  
+
+  coffeeDataFromRedux = useSelector(state => state.coffeeReducer.recommendedList[0])
+  console.log(coffeeDataFromRedux)
   return (
     <>
     
@@ -792,6 +808,44 @@ const App = () => {
     <h1 className="text-3xl font-cursive underline">
       Hello world!
     </h1>
+    <div className="flex flex-wrap">
+      {console.log(coffeeDataFromRedux)}
+    {coffeeDataFromRedux.length !== 0 ? 
+    
+  
+    coffeeDataFromRedux.map(coffee => {
+      return (
+
+          <div className="w-full max-w-sm bg-white rounded-lg m-2">
+              <a href="#">
+              
+                  <img className="p-8 rounded-full" src={coffee.recipe.images.REGULAR.url} alt="product image" />
+              </a>
+              <div className="px-5 pb-5">
+                  <a href="#">
+                      <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{coffee.recipe.label}</h5>
+                  </a>
+                  <div className="flex items-center mt-2.5 mb-5">
+                    {coffee.recipe.dietLabels.map(label => {
+                      return (
+                        <button class="bg-pink-300 hover:bg-pink-500 text-white font-serif py-2 px-4 mr-3 rounded-full">
+                          {label}
+                        </button> 
+                      )
+                    })}
+                  </div>
+                
+              </div>
+          </div>
+
+      )
+    })
+   
+    :
+    <></>
+  
+  }
+  </div>
     
     </>
   )
