da0c89684acc646873e7ac5b74bd8cf333fc00a6
Joy He <joyhe520@gmail.com>
2023-06-21 11:58:28 -0500

    found chosen coffee

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector} from 'react-redux'
 
 const Details = () => {
+    //get id from the prop
     const location = useLocation()
     const id = location.state
-    console.log(id)
+    //get all the recommended coffee from redux and filter to find the one that was clicked
+    const coffeeFromRedux = useSelector(state => state.coffeeReducer.recommendedList)[0]
+    const chosenCoffee = coffeeFromRedux.filter(coffee => coffee.recipe.id === id)
+    console.log(chosenCoffee)
   return (
     <div>
 
