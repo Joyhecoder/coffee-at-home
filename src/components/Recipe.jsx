098a46b71c17acb6054ba27a4fd06388a8bb1821
Joy He <joyhe520@gmail.com>
2023-06-22 14:15:46 -0500

    create new redux for search and pull data in the recipe component

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useDispatch, useSelector} from 'react-redux'
 
 const Recipe = () => {
+  const searchedCoffee = useSelector(state => state.coffeeReducer.search)
+  const searchedCoffeeName = useSelector(state => state.coffeeReducer.searchedName)
+  console.log(searchedCoffeeName)
+  console.log(searchedCoffee)
+
+  let coffeeDataFromRedux 
+  if(searchedCoffeeName in searchedCoffee){
+    coffeeDataFromRedux = searchedCoffee[searchedCoffeeName]
+  }
+  console.log(coffeeDataFromRedux)
   return (
     <div>Recipe</div>
   )
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,8 @@
 import {  useState } from 'react'
+import axios from "axios";
+import { useDispatch, useSelector} from 'react-redux'
+import { search_coffee, update_search_name } from '../reducers/exampleSlice';
+import { v4 as uuidv4 } from 'uuid';
 import { Dialog,  Popover} from '@headlessui/react'
 import {
   ArrowPathIcon,
@@ -28,8 +32,45 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
+
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [coffee, setCoffee] = useState('')
+  const dispatch = useDispatch()
+
+  const searchedCoffee = useSelector(state => state.coffeeReducer.search)
+  
+  // search recipe function
+  const handleSearch = async (e) => {
+    e.preventDefault()
+    console.log(coffee)
+    //fetch data
+    try {
+      const response = await fetch('http://localhost:8000/api')
+      const data = await response.json()
+      let apiKey = data.api
+      console.log(searchedCoffee)
+      //check if the keyword is saved in redux
+      if(searchedCoffee[coffee] === undefined){
+        console.log("not in redux")
+        let fetchCoffeeData = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${coffee}&app_id=14295534&app_key=${apiKey}`)
+        console.log(fetchCoffeeData.data.hits)
+        const exampleData = fetchCoffeeData.data.hits
+        //create id for each data
+        exampleData.forEach(data => {
+          data.recipe["id"] = uuidv4()
+        })
+
+        dispatch(search_coffee([coffee, exampleData]))
+      }
+      dispatch(update_search_name(coffee))
+
+
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <header className="bg-white">
@@ -65,8 +106,8 @@ export default function Header() {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <div className="pt-2 relative mx-auto text-gray-600">
             <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-              type="search" name="search" placeholder="Search" />
-            <button type="submit" className="absolute right-4 top-0 mt-5 ml-3">
+              type="search" name="search" placeholder="Search" onChange={e=>setCoffee(e.target.value)}/>
+            <button type="submit" className="absolute right-4 top-0 mt-5 ml-3" onClick={handleSearch}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-gray-600 h-4 w-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"  />
               </svg>
@@ -125,7 +166,7 @@ export default function Header() {
                 <div className="pt-2 relative mx-auto text-gray-600">
                   <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                     type="search" name="search" placeholder="Search" />
-                  <button type="submit" className="absolute right-4 top-0 mt-5 ml-3">
+                  <button type="submit" className="absolute right-4 top-0 mt-5 ml-3" onClick={handleSearch}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-gray-600 h-4 w-4">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"  />
                     </svg>
